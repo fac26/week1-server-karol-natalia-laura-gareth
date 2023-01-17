979c4634e949ec85977c21847f3c7d0ca7232b9e
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-01-17 17:41:29 +0000

    Relates #1 Relates #11 Co-authored-by: Karol <Kr33L@users.noreply.github.com> Co-authored-by: Laura Keating <LauraK0@users.noreply.github.com> Co-authored-by: Gal K Jones <GalKJ@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -1,10 +1,40 @@
 const express = require('express');
-// const {home} = require
+
+
 const server = express();
+const bodyParser = express.urlencoded({extended: false});
+
+//custom modules
+const {html} = require('./template');
+const {validate}=require('./validate');
+
+
+//static
+const staticHandler = express.static("public");
+server.use(staticHandler);
+
+//code
+const posts=[];
+
 
 server.get('/', (req, res) => {
-    res.send('Hello Big World!');
+    res.send(`Hello Big World! ${html(posts)}`);
 });
+
+server.post('/', bodyParser, (req, res)=>{
+    const {title, author, message} = req.body;   
+
+
+    //test here for error and generate err obj
+    if(!validate(title)){
+        console.log('invalid')
+    }else{
+        posts.push({title: title, author: author, message:message, id: 'x'});
+    }
+    
+    
+    res.redirect('/');
+})
 
 module.exports = server;
 // Always keep module.exports at the bottom of the file
---
+++ src/template.js
@@ -0,0 +1,78 @@
+
+
+const DUMMY_POSTS = [
+    {
+        title: 't1',
+        author: 'author1',
+        message: 'post1',
+        id: '1',
+        username: 'GB',
+    },
+    {
+        title: 't2',
+        author: 'author1',
+        message: 'post1',
+        id: '2',
+        username: 'GB',
+    }
+];
+
+function renderPost(post){
+    return `<li>
+    <h3>${post.title}</h3>
+    <p>${post.author}</p>
+    <p>${post.message}</p>
+    </li>`
+}
+
+function renderPosts(posts){//DUMMY_POSTS
+    return posts.map(post=>renderPost(post));//['<li>..', '<li>']
+
+}
+
+function renderForm(){//err:{title: true||false, author, message} value:{title, author, message}
+
+    return `<form method="POST">
+    <div>
+    <label for="title">Enter title</label>
+    <input type="text" name="title" id="title"}/>
+    
+    </div>
+    <div>
+    <label for="author">Enter author name</label>
+    <input type="text" name="author" id="author"}/>
+    
+    </div>
+    <div>
+    <label for="message">Enter title</label>
+    <input type="text" name="message" id="message"/>
+   
+    </div>
+    <button type="submit">Submit</button>
+    </form>`
+
+}
+
+
+
+function html(posts, err, values){
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/style.css">
+        <title>Document</title>
+    </head>
+    <body>
+    ${renderForm(err, values)}
+    <h2>Haikuniverse posts</h2>
+    <ul class='dummy-post'>${renderPosts(DUMMY_POSTS).join('')}</ul>
+    <ul class='users-post'>${renderPosts(posts)}</ul>
+        
+    </body>
+    </html>`
+}
+
+module.exports = {html}
